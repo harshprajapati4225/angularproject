62b7228ca97d1440fd3aa2335ee9d809ab65470d
harshprajapati4225 <skey.harshprajapti@gmail.com>
2023-03-16 11:34:42 +0530

    ahscv

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/app/login-page/*.html","./src/app/profile/*.html","./src/app/teamleader-header/*.html","./src/app/register/*.html","./src/app/team-leader/*.html","./src/app/tasks/*.html","./src/app/team-member/*.html",
-  "./src/app/edit-members/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
